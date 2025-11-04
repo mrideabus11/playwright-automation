31ca3fc01890af3a7b08288d2e85c33f6748735e
Pradyumna Subedi <pradyumnasubedi@gmail.com>
2025-11-04 13:12:56 +0545

    Learn Hooks and Groups: login steps repeated

--- tests/hooksAndGroups.spec.js
+++ tests/hooksAndGroups.spec.js
@@ -1,14 +1,15 @@
 import { test, expect } from "@playwright/test"
 
-test("test", async ({ page }) => {
+test("login", async ({ page }) => {
   await page.goto("https://www.saucedemo.com")
   await page.pause()
   await page.locator('[data-test="username"]').fill("standard_user")
   await page.locator('[data-test="password"]').fill("secret_sauce")
   await page.locator('[data-test="login-button"]').click()
+  await page.close()
 })
 
-test.only("Homepage", async ({ page }) => {
+test("Homepage", async ({ page }) => {
   //here .only is an annotation
   await page.goto("https://www.saucedemo.com")
   await page.locator('[data-test="username"]').fill("standard_user")
