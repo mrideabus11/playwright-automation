019c511c563f050dbc81474531d3a1ab64175320
Pradyumna Subedi <pradyumnasubedi@gmail.com>
2025-11-04 13:51:41 +0545

    Learn Hooks and Groups: Groups

--- tests/hooksAndGroups.spec.js
+++ tests/hooksAndGroups.spec.js
@@ -1,24 +1,28 @@
 import { test, expect } from "@playwright/test"
 
-test.beforeEach(async ({ page }) => {
-  await page.goto("https://www.saucedemo.com")
-  await page.pause()
-  await page.locator('[data-test="username"]').fill("standard_user")
-  await page.locator('[data-test="password"]').fill("secret_sauce")
-  await page.locator('[data-test="login-button"]').click()
-  await page.waitForURL("https://www.saucedemo.com/inventory.html")
-})
+test.describe("Saucedemo.com testes", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("https://www.saucedemo.com")
+    await page.pause()
+    await page.locator('[data-test="username"]').fill("standard_user")
+    await page.locator('[data-test="password"]').fill("secret_sauce")
+    await page.locator('[data-test="login-button"]').click()
+    await page.waitForURL("https://www.saucedemo.com/inventory.html")
+  })
 
-test("Homepage", async ({ page }) => {
-  //here .only is an annotation
+  test("Homepage", async ({ page }) => {
+    //here .only is an annotation
 
-  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click()
-  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
-  await page.locator('[data-test="item-1-title-link"]').click()
-  await page.locator('[data-test="add-to-cart"]').click()
-})
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    await page
+      .locator('[data-test="add-to-cart-sauce-labs-bike-light"]')
+      .click()
+    await page.locator('[data-test="item-1-title-link"]').click()
+    await page.locator('[data-test="add-to-cart"]').click()
+  })
 
-test("logout", async ({ page }) => {
-  await page.getByRole("button", { name: "Open Menu" }).click()
-  await page.locator('[data-test="logout-sidebar-link"]').click()
+  test("logout", async ({ page }) => {
+    await page.getByRole("button", { name: "Open Menu" }).click()
+    await page.locator('[data-test="logout-sidebar-link"]').click()
+  })
 })
