8d9623cd0598d8d341ac2bcf0bc2af83d44d527d
Pradyumna Subedi <pradyumnasubedi@gmail.com>
2025-11-04 13:40:35 +0545

    Learn Hooks and Groups: Hooks(BeforeEach & AfterAll)

--- tests/hooksAndGroups.spec.js
+++ tests/hooksAndGroups.spec.js
@@ -1,35 +1,24 @@
 import { test, expect } from "@playwright/test"
 
-test("login", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("https://www.saucedemo.com")
   await page.pause()
   await page.locator('[data-test="username"]').fill("standard_user")
   await page.locator('[data-test="password"]').fill("secret_sauce")
   await page.locator('[data-test="login-button"]').click()
-  await page.close()
+  await page.waitForURL("https://www.saucedemo.com/inventory.html")
 })
 
 test("Homepage", async ({ page }) => {
   //here .only is an annotation
-  await page.goto("https://www.saucedemo.com")
-  await page.locator('[data-test="username"]').fill("standard_user")
-  await page.locator('[data-test="password"]').fill("secret_sauce")
-  await page.locator('[data-test="login-button"]').click()
-  await page.waitForURL("https://www.saucedemo.com/inventory.html")
 
   await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click()
   await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click()
   await page.locator('[data-test="item-1-title-link"]').click()
   await page.locator('[data-test="add-to-cart"]').click()
-  await page.close()
 })
 
-test("logout", async ({ oage }) => {
-  await page.goto("https://www.saucedemo.com")
-  await page.locator('[data-test="username"]').fill("standard_user")
-  await page.locator('[data-test="password"]').fill("secret_sauce")
-  await page.locator('[data-test="login-button"]').click()
-  await page.waitForURL("https://www.saucedemo.com/inventory.html")
+test("logout", async ({ page }) => {
   await page.getByRole("button", { name: "Open Menu" }).click()
   await page.locator('[data-test="logout-sidebar-link"]').click()
 })
